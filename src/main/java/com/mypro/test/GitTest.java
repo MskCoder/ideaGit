7679ab46f30c61e0bd5349367761b17bad413039
MskCoder <yhh1286049040@gmail.com>
2022-06-16 09:34:09 +0800

    master 分支1.0

--- src/main/java/com/mypro/test/GitTest.java
+++ src/main/java/com/mypro/test/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hi,Git!");
         System.out.println("Git test01");
+        System.out.println("master 分支1.0");
     }
 }
