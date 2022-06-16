b618e247c06af86fd17020a38eef9588e67a514e
MskCoder <yhh1286049040@gmail.com>
2022-06-16 09:28:03 +0800

    idea 提交代码 2.0

--- src/main/java/com/mypro/test/GitTest.java
+++ src/main/java/com/mypro/test/GitTest.java
@@ -3,5 +3,6 @@ package com.mypro.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hi,Git!");
+        System.out.println("Git test01");
     }
 }
